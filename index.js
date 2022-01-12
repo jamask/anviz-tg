b61771d3ffe3d658f76fc333761db4efd27b50c9
Askhat Zhamabayev <jam.askhat@gmail.com>
2022-01-13 00:46:09 +0600

    feat: add mssql packet

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const TelegramBot = require('node-telegram-bot-api');
+const sql = require('mssql');
 
 const token = '5018400836:AAHw90GzgFBkAdVXiYSgTPyBdMDfCg1BGDk';
 
 const bot = new TelegramBot(token, {polling: true});
+
+const sqlConfig = {
+  user: 'sa',
+  password: 'Aa123456',
+  database: 'testanviz',
+  server: 'localhost',
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true // change to true for local dev / self-signed certs
+  }
+}
+
+sql.connect(sqlConfig).then(pool => {
+  // Query
+  
+  return pool.request()
+      .query('select * from Records')
+}).then(result => {
+  console.dir(result)
+}).catch(err => {
+  console.error(err.message)
+});
 
 bot.onText(/\/start/, (msg, _) => {
   const chatId = msg.chat.id;
@@ -18,3 +46,4 @@ ID: ${msg.from.id}
   bot.sendMessage(chatId, returnMessage);
   console.log(msg)
 });
+
